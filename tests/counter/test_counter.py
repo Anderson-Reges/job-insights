796aefa28f53238be5b6d4759b1596d9b24a0285
Anderson-Reges <andersonreges76@gmail.com>
2023-04-28 01:41:50 -0300

    test: implementando a test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    assert count_ocurrences(path, 'miN') == 6324
+    assert count_ocurrences(path, 'Max') == 248
