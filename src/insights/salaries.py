b6f8f27831fbe3bcf9769e934805069356625ccf
Anderson-Reges <andersonreges76@gmail.com>
2023-04-28 01:16:34 -0300

    fix: consertando a função matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -21,17 +21,43 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    if "min_salary" not in job or "max_salary" not in job:
-        raise ValueError("'min_salary' e/ou 'max_salary' ausentes")
-    if not (
-        str(job["min_salary"]).isdigit() and str(job["max_salary"]).isdigit()
-    ):
-        raise ValueError("'min_salary' e/ou 'max_salary' não são dígitos")
-    if int(job["min_salary"]) > int(job["max_salary"]):
-        raise ValueError("'min_salary' é maior que 'max_salary'")
-    if not str(salary).isdigit():
-        raise ValueError("'salary' não é um dígito")
-    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
+    try:
+        min_salary, max_salary = verify_values(job)
+    except ValueError:
+        raise ValueError
+    if min_salary > max_salary:
+        raise ValueError("O 'min_salary' deve ser menor que o 'max_salary'")
+
+    return min_salary <= verify_salary(salary) <= max_salary
+
+
+def verify_values(job: Dict) -> Dict:
+    if 'min_salary' not in job or 'max_salary' not in job:
+        raise ValueError("'min_salary' e 'max_salary' estão ausentes em job")
+    verify_salary_type(job)
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+    except ValueError:
+        raise ValueError("'min_salary' ou 'max_salary' não tem valor numerico")
+
+    return [min_salary, max_salary]
+
+
+def verify_salary(salary: Union[int, str]):
+    if not isinstance(salary, (int, str)):
+        raise ValueError('tipo invalido')
+    try:
+        return int(salary)
+    except ValueError:
+        raise ValueError("O 'salary' deve ser numero")
+
+
+def verify_salary_type(job: Dict):
+    if not isinstance(job['min_salary'], (str, int)):
+        raise ValueError('tipo invalido')
+    if not isinstance(job['max_salary'], (str, int)):
+        raise ValueError('tipo invalido')
 
 
 def filter_by_salary_range(
